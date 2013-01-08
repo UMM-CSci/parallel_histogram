c77bf711ba9e5f655859faa28a7d7dfba0e85b37
Nic McPhee <mcphee@morris.umn.edu>
2013-01-07 19:52:58 -0600

    Initial version with threads. Broken because (probably among other things) it doesn't wait for all the threads to complete.

--- src/histogram/HistogramBuilder.java
+++ src/histogram/HistogramBuilder.java
@@ -1,16 +1,38 @@
 package histogram;
 
-
 public class HistogramBuilder {
-
+    public final int NUM_THREADS = 10;
+    
     public int[] buildHistogram(String string) {
         int[] result = new int[128];
         
-        for (char c : string.toCharArray()) {
-            ++result[c];
+        for (int i=0; i<NUM_THREADS; ++i) {
+            PartialHistogramBuilder partial = new PartialHistogramBuilder(string, result, i);
+            Thread thread = new Thread(partial);
+            thread.start();
         }
         
         return result;
     }
 
+    class PartialHistogramBuilder implements Runnable {
+        private String string;
+        private int[] result;
+        private int blockNumber;
+
+        public PartialHistogramBuilder(String string, int[] result, int blockNumber) {
+            this.string = string;
+            this.result = result;
+            this.blockNumber = blockNumber;
+        }
+
+        @Override
+        public void run() {
+          int start = string.length() / NUM_THREADS * blockNumber;
+          int end = string.length() / NUM_THREADS * (blockNumber + 1);
+          for (int i=start; i<end; ++i) {
+              ++result[string.charAt(i)];
+          }
+        }
+    }
 }
