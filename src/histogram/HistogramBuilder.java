7306e31f77d9f09b8a15762314dbda0194ba1281
Nic McPhee <mcphee@morris.umn.edu>
2013-01-17 13:42:05 -0600

    Initial attempt at threading which still doesn't work because we don't have any locking.

--- src/histogram/HistogramBuilder.java
+++ src/histogram/HistogramBuilder.java
@@ -1,15 +1,49 @@
 package histogram;
 
 public class HistogramBuilder {
+	public final int NUM_THREADS = 10; // This is also the # of blocks
     
     public int[] buildHistogram(String string) {
         int[] result = new int[128];
+        Thread[] threads = new Thread[NUM_THREADS];
         
-        for (char c : string.toCharArray()) {
-            ++result[c];
+        for (int i=0; i<NUM_THREADS; ++i) {
+        	PartialHistogramBuilder builder = new PartialHistogramBuilder(string, i, result);
+        	threads[i] = new Thread(builder);
+        	threads[i].start();
         }
-        
+        for (int i=0; i<NUM_THREADS; ++i) {
+        	try {
+				threads[i].join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        }
         return result;
+    }
+    
+    class PartialHistogramBuilder implements Runnable {
+    	
+    	private String string;
+		private int[] result;
+		private int blockNumber;
+
+		public PartialHistogramBuilder(String string, int blockNumber, int[] result) {
+    		this.string = string;
+    		this.result = result;
+    		this.blockNumber = blockNumber;
+    	}
+
+		@Override
+		public void run() {
+			int start = (blockNumber * string.length()) / NUM_THREADS;
+			int end = ((blockNumber + 1) * string.length()) / NUM_THREADS;
+			for (int i=start; i<end; ++i) {
+				++result[string.charAt(i)];
+			}
+		}
+    	
     }
     
     public static void main(String[] args) {
